0f46b5ddad99e59a6f48e1761cfcaf8139ba30cf
Educacion <educacion@creativa.com>
2019-07-08 14:52:14 -0600

    proyecto terminado

---
+++ cuentaBancaria/src/cuentabancaria/CuentaBancaria.java
@@ -0,0 +1,84 @@
+
+package cuentabancaria;
+
+import java.util.Scanner;
+import java.util.Set;
+
+/**
+ *
+ * @author educacion
+ */
+public class CuentaBancaria {
+
+    private String nombreCliente;
+    private String numeroCuenta;
+    private double interes;
+    private double saldo;
+
+    public CuentaBancaria() {
+    }
+    
+    public CuentaBancaria(String nombreCliente, String numeroCuenta, double interes, double saldo) {
+        this.nombreCliente = nombreCliente;
+        this.numeroCuenta = numeroCuenta;
+        this.interes = interes;
+        this.saldo = saldo;
+    }
+    
+    public static void main(String[] args) {
+        
+        Scanner sc = new Scanner(System.in);
+        CuentaBancaria cuenta = new CuentaBancaria();
+        System.out.println("Ingrese los sieguietes campos: ");
+        System.out.println("Cliente: ");
+        cuenta.setNombreCliente(sc.next());
+        System.out.println("Cuenta: ");
+        cuenta.setNumeroCuenta(sc.next());
+        System.out.println("Interes");
+        cuenta.setInteres(sc.nextDouble());
+        System.out.println("Saldo: ");
+        cuenta.setSaldo(sc.nextDouble());
+        
+        if(null!=cuenta){
+            System.out.println("Nombre cliente: "+cuenta.getNombreCliente());
+            System.out.println("Nombre cuenta: "+cuenta.getNumeroCuenta());
+            System.out.println("Nombre interes: "+cuenta.getInteres());
+            System.out.println("Nombre saldo: "+cuenta.getSaldo());
+        }
+    }
+
+    public String getNombreCliente() {
+        return nombreCliente;
+    }
+
+    public void setNombreCliente(String nombreCliente) {
+        this.nombreCliente = nombreCliente;
+    }
+
+    public String getNumeroCuenta() {
+        return numeroCuenta;
+    }
+
+    public void setNumeroCuenta(String numeroCuenta) {
+        this.numeroCuenta = numeroCuenta;
+    }
+
+    public double getInteres() {
+        return interes;
+    }
+
+    public void setInteres(double interes) {
+        this.interes = interes;
+    }
+
+    public double getSaldo() {
+        return saldo;
+    }
+
+    public void setSaldo(double saldo) {
+        this.saldo = saldo;
+    }
+    
+    
+
+}
